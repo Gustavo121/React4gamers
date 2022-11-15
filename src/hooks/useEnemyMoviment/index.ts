5a5d280c77c2bba10197f0e01e0f880f05cc0648
Gustavo <gustgouvea@hotmail.com>
2022-11-15 13:12:35 -0300

    refatoração de movimento

--- src/hooks/useEnemyMoviment/index.ts
+++ src/hooks/useEnemyMoviment/index.ts
@@ -1,5 +1,6 @@
 import useInterval from '@use-it/interval';
 import React from 'react';
+import { handleNextMoviment } from '../../contexts/canvas/helpers';
 import { EDirection } from '../../settings/constants';
 
 function useEnemyMoviment(initialPosition) {
@@ -11,17 +12,7 @@ function useEnemyMoviment(initialPosition) {
     var directionArray = Object.values(EDirection);
     const randomDirection = directionArray [random];
 
-    if (randomDirection === EDirection.LEFT) {
-      updatePositionState({ x: positionState.x - 1, y: positionState.y });
-      updateDirectionState(EDirection.LEFT);
-    } else if (randomDirection === EDirection.RIGHT) {
-      updatePositionState({ x: positionState.x + 1, y: positionState.y });
-      updateDirectionState(EDirection.RIGHT);
-    } else if (randomDirection === EDirection.DOWN) {
-      updatePositionState({ x: positionState.x, y: positionState.y - 1 });
-    } else if (randomDirection === EDirection.UP) {
-      updatePositionState({ x: positionState.x, y: positionState.y + 1 });
-    }
+    handleNextMoviment(randomDirection, positionState)
   }, 2000);
 
   return {
